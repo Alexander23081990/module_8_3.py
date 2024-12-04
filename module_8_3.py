d694705c40137940a47fc8b75775d74d66b0ef56
Alexander23081990 <95206213+Alexander23081990@users.noreply.github.com>
2024-12-04 12:01:41 +0300

    Update module_8_3.py(исправленное)

--- module_8_3.py
+++ module_8_3.py
@@ -17,15 +17,15 @@ class Car:
     def __is_valid_vin(self, vin_number):
         if not isinstance(vin_number, int):
             raise IncorrectVinNumber('Некорректный тип vin номера')
-        if vin_number not in range(1000000, 10000000):
+        if not 1000000 <= vin_number <= 9999999:
             raise IncorrectVinNumber('Неверный диапазон для vin номера')
         return vin_number
 
     def __is_valid_numbers(self, numbers):
         if not isinstance(numbers, str):
-            raise IncorrectVinNumber('Некорректный тип данных для номеров')
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
         if len(numbers) != 6:
-            raise IncorrectVinNumber('Неверная длина номера')
+            raise IncorrectCarNumbers('Неверная длина номера')
         return numbers
 
 
